f34d58f4f1345952579181bef49f3de9f2137b16
Tiago Relvao <relvao@gmail.com>
2015-04-22 19:35:30 +0100

    add notes

--- modules/people/manifests/relvao.pp
+++ modules/people/manifests/relvao.pp
@@ -1,3 +1,14 @@
 class people::relvao {
   include people::relvao::applications
+
+	# Set git credentials
+
+    include git
+
+    git::config::global { 'user.email':
+        value  => 'rob@enigma-marketing.co.uk'
+    }
+    git::config::global { 'user.name' :
+        value  => 'Rob McCardle'
+    }
 }
