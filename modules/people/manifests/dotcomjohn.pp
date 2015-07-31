96fb6b8ac8ea051248c88418b9f9ecff99960e95
enigmamkt <admin@enigma-marketing.co.uk>
2015-07-31 10:20:36 +0100

    Updating Steve and Stretch manifests
    Changes to be committed:
    	modified:   modules/people/manifests/dotcomjohn.pp
    	modified:   modules/people/manifests/stvwlkr.pp

--- modules/people/manifests/dotcomjohn.pp
+++ modules/people/manifests/dotcomjohn.pp
@@ -1,3 +1,14 @@
 class people::dotcomjohn {
   include people::dotcomjohn::applications
+
+	# Set git credentials
+
+    include git
+
+    git::config::global { 'user.email':
+        value  => ‘johndc@enigma-marketing.co.uk'
+    }
+    git::config::global { 'user.name' :
+        value  => ‘John de Clerck’
+    }
 }
--- modules/people/manifests/stvwlkr.pp
+++ modules/people/manifests/stvwlkr.pp
@@ -1,3 +1,14 @@
 class people::stvwlkr {
   include people::stvwlkr::applications
+
+	# Set git credentials
+
+    include git
+
+    git::config::global { 'user.email':
+        value  => ‘steve@enigma-marketing.co.uk'
+    }
+    git::config::global { 'user.name' :
+        value  => ‘Steve Walker’
+    }
 }
